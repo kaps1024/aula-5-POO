97ee12fb6be060155ff7ad3e18fc0ed1d9fe4368
kaps1024 <kapsseind@gmail.com>
2026-03-23 20:24:51 -0300

    Update ItemAtendimento.java
    correção final atividade

--- Atividade aula 5/ItemAtendimento.java
+++ Atividade aula 5/ItemAtendimento.java
@@ -1,3 +1,24 @@
 public class ItemAtendimento {
-    
+    private AplicavelAoAnimal item; // Uso de Interface
+    private int quantidade;
+    private double precoUnitarioSnapshot; // Snapshot de preço 
+    private double descontoPercent;
+
+    public ItemAtendimento(AplicavelAoAnimal item, int quantidade, double descontoPercent) {
+        this.item = item;
+        this.quantidade = quantidade;
+        this.precoUnitarioSnapshot = item.getPreco(); // Congela o preço no momento da criação
+        this.descontoPercent = descontoPercent;
+    }
+
+    public double getSubtotalBruto() {
+        return precoUnitarioSnapshot * quantidade;
+    }
+
+    public double getSubtotalComDesconto() {
+        return getSubtotalBruto() * (1 - (descontoPercent / 100));
+    }
+
+    public AplicavelAoAnimal getItem() { return item; }
+    public int getQuantidade() { return quantidade; }
 }
