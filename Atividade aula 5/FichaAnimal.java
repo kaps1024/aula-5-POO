3fff673e9ed9b1f087775d7c99007d5640c0068b
kaps1024 <kapsseind@gmail.com>
2026-03-23 20:24:31 -0300

    Update FichaAnimal.java

--- Atividade aula 5/FichaAnimal.java
+++ Atividade aula 5/FichaAnimal.java
@@ -1,3 +1,29 @@
 public class FichaAnimal {
-    
+    private String especie;
+    private String raca;
+    private int idade;
+    private String porte;
+
+    public FichaAnimal(String especie, String raca, int idade, String porte) {
+        this.especie = especie;
+        this.raca = raca;
+        this.idade = idade;
+        this.porte = porte;
+    }
+
+    public FichaAnimal() {}
+
+    public String getEspecie() { return especie; }
+    public String getRaca() { return raca; }
+    public int getIdade() { return idade; }
+    public String getPorte() { return porte; }
+
+    public void setEspecie(String especie) { this.especie = especie; }
+    public void setRaca(String raca) { this.raca = raca; }
+    public void setIdade(int idade) { this.idade = idade; }
+    public void setPorte(String porte) { this.porte = porte; }
+
+    public String resumo() {
+        return "Espécie: " + especie + ", Raça: " + raca + ", Idade: " + idade + ", Porte: " + porte;
+    }
 }
