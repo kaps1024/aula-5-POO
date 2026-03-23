4d4a38aca650b9d2125b492111b5f5b77c9a4975
kaps1024 <kapsseind@gmail.com>
2026-03-23 20:23:43 -0300

    Update AplicavelAoAnimal.java
    correção final da atividade

--- Atividade aula 5/AplicavelAoAnimal.java
+++ Atividade aula 5/AplicavelAoAnimal.java
@@ -1,3 +1,5 @@
 public interface AplicavelAoAnimal {
-    
+    boolean aplicavel(FichaAnimal a);
+    String getDescricao();
+    double getPreco(); //Necessário para o snapshot de preço no ItemAtendimento
 }
