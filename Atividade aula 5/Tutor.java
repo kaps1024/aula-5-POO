c41cc192fec9d8a00f0f59ab8d06e89fc612e36d
kaps1024 <kapsseind@gmail.com>
2026-03-23 20:26:41 -0300

    Update Tutor.java
    correção final atividade

--- Atividade aula 5/Tutor.java
+++ Atividade aula 5/Tutor.java
@@ -1,13 +1,17 @@
 public class Tutor {
-    public int id;
-    public String nome;
-    public String CPF;
-    public FichaAnimal animal;
+    private int id;
+    private String nome;
+    private String CPF;
+    private FichaAnimal animal;
 
     public Tutor(int id, String nome, String CPF, FichaAnimal animal) {
         this.id = id;
         this.nome = nome;
         this.CPF = CPF;
         this.animal = animal;
+    }
+
+    public FichaAnimal getAnimal() {
+        return animal;
     }
 }
